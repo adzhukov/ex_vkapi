cf9af64ee50d59af5a539d0bb1da40391fd2e575
Alexander Zhukov <36170455+adzhukov@users.noreply.github.com>
2020-06-26 21:32:43 +0300

    Add execute method

--- lib/vkapi.ex
+++ lib/vkapi.ex
@@ -1,3 +1,10 @@
 defmodule VKAPI do
   use VKAPI.Macro
+
+  @doc """
+  A universal method for calling a sequence of other methods while saving and filtering interim results.
+  """
+  def execute(code) do
+    VKAPI.Request.request("execute", code)
+  end
 end
