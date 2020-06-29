0e1f3462bd3ad40821d4dba307ba130355daa8bc
Alexander Zhukov <36170455+adzhukov@users.noreply.github.com>
2020-06-29 12:47:40 +0300

    Make execute method accept string as code

--- lib/vkapi.ex
+++ lib/vkapi.ex
@@ -4,6 +4,10 @@ defmodule VKAPI do
   @doc """
   A universal method for calling a sequence of other methods while saving and filtering interim results.
   """
+  def execute(code) when is_binary(code) do
+    execute(%{code: code})
+  end
+
   def execute(code) do
     VKAPI.Request.request("execute", code)
   end
